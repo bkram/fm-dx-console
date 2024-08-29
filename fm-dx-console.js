1d4442774eac4251dc7714ef69ac1a7615db7e2b
Mark de Bruijn <mrdebruijn@gmail.com>
2024-08-29 22:22:41 +0200

    fix: v1.2.7 fix

--- fm-dx-console.js
+++ fm-dx-console.js
@@ -357,8 +357,8 @@ function updateTunerBox(jsonData) {
         `${padStringWithSpaces("Freq:", 'green', padLength)}${jsonData.freq} Mhz\n` +
         `${padStringWithSpaces("Signal:", 'green', padLength)}${parseFloat(jsonData.sig).toFixed(1)} dBf\n` +
         `${padStringWithSpaces("Mode:", 'green', padLength)}${jsonData.st ? "Stereo" : "Mono"}\n` +
-        `${padStringWithSpaces("iMS:", 'green', padLength)}${jsonData.ims ? "On" : "{grey-fg}Off{/grey-fg}"}\n` +
-        `${padStringWithSpaces("EQ:", 'green', padLength)}${jsonData.eq ? "On" : "{grey-fg}Off{/grey-fg}"}\n` +
+        `${padStringWithSpaces("iMS:", 'green', padLength)}${Number(jsonData.ims) ? "On" : "{grey-fg}Off{/grey-fg}"}\n` +
+        `${padStringWithSpaces("EQ:", 'green', padLength)}${Number(jsonData.eq) ? "On" : "{grey-fg}Off{/grey-fg}"}\n` +
         `${padStringWithSpaces("ANT:", 'green', padLength)}${antNames[jsonData.ant]}\n`);
 }
 
